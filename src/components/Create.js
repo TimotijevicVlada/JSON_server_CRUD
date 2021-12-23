7c205f01ae068cbce2b922212ca225f36579ed2f
Vladimir <timotijevicvlada@gmail.com>
2021-12-23 08:25:27 +0100

    Formik and validation added.

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,26 +1,80 @@
-import React, {useRef, useContext} from "react";
+import React, { useRef, useContext, useState } from "react";
 import { CrudContext } from "../context/Context";
+import { useFormik } from "formik";
+import axios from "axios";
 
 const Create = () => {
 
-  const {setCreateVisible, createValues, setCreateValues, handleAdd} = useContext(CrudContext);
+  const [successMessage, setSuccessMesage] = useState(false);
+  const { setCreateVisible, getData} = useContext(CrudContext);
   const formRef = useRef();
 
   //Function that exit the form when we click out of the form
   const handleExit = (e) => {
     if (!formRef.current.contains(e.target)) {
-        setCreateVisible(false);
+      setCreateVisible(false);
     }
   };
 
+   //Custom form validation
+    const validate = (values) => {
+    const errors = {};
+    if (!values.title) {
+      errors.title = "Title is required!";
+    } 
+    if(!values.author) {
+        errors.author = "Author is required!";
+    }
+    return errors;
+  };
+
+    //Formik library
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      author: ""
+    },
+    validate,
+    onSubmit: async (values) => {
+        const newPost = {
+            title: values.title,
+            author: values.author
+        }
+        await axios.post("http://localhost:3006/posts", newPost);
+        getData();
+        setSuccessMesage(true);
+    }
+  });
+
   return (
     <div onClick={handleExit} className="create_wrapper">
-      <form onSubmit={handleAdd} ref={formRef}>
+      <form onSubmit={formik.handleSubmit} ref={formRef}>
         <div className="create_title">Create</div>
         <div className="create_inputs">
-          <input onChange={(e) => setCreateValues({...createValues, title: e.target.value})} type="text" placeholder="Title" />
-          <input onChange={(e) => setCreateValues({...createValues, author: e.target.value})} type="text" placeholder="Author" />
+          <input
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
+            type="text"
+            placeholder="Title"
+            name="title"
+          />
+          {formik.touched.title && formik.errors.title && (
+            <div className="error">{formik.errors.title}</div>
+          )}
+          <input
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.author}
+            type="text"
+            placeholder="Author"
+            name="author"
+          />
+          {formik.touched.author && formik.errors.author && (
+            <div className="error">{formik.errors.author}</div>
+          )}
           <button type="submit">Create</button>
+          {successMessage && <div className="success_message">Post has been created!</div>}
         </div>
       </form>
     </div>
--- src/context/Context.js
+++ src/context/Context.js
@@ -4,10 +4,9 @@ import axios from "axios";
 export const CrudContext = createContext();
 
 export const ContextProvider = ({ children }) => {
-  
+
   const [data, setData] = useState([]);
   const [createVisible, setCreateVisible] = useState(false);
-  const [createValues, setCreateValues] = useState({});
 
   //Function that get the data from database
   const getData = async () => {
@@ -20,22 +19,14 @@ export const ContextProvider = ({ children }) => {
     getData();
   }, []);
 
-  //Handle add new post
-  const handleAdd = async () => {
-    await axios.post("http://localhost:3006/posts", createValues);
-    getData();
-  };
 
   return (
     <CrudContext.Provider
       value={{
         data,
         getData,
-        handleAdd,
         createVisible,
-        setCreateVisible,
-        createValues,
-        setCreateValues,
+        setCreateVisible
       }}
     >
       {children}
